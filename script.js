334430efcc3fcd4ada56f26a15a5f38d649c06c1
Skillhorde <abew2001@gmail.com>
2026-03-29 02:19:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ function openLightbox(src) {
   const img = document.getElementById("lightbox-img");
 
   img.src = src;
+
   lightbox.style.display = "flex";
+
+  // allow browser to register display change before animation
+  setTimeout(() => {
+    lightbox.classList.add("active");
+  }, 10);
 }
 
 function closeLightbox() {
-  document.getElementById("lightbox").style.display = "none";
+  const lightbox = document.getElementById("lightbox");
+
+  lightbox.classList.remove("active");
+
+  // wait for animation to finish before hiding
+  setTimeout(() => {
+    lightbox.style.display = "none";
+  }, 300);
 }
 
 let lastScroll = 0;
